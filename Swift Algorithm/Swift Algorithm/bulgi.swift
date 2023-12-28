bf9d3e89133d9a6174b56abfb242a2959214c942
unuhqueen <csw98213@gmail.com>
2023-12-28 17:46:06 +0900

    18108번: 1998년생인 내가 태국에서는 2541년생?!

---
+++ Swift Algorithm/Swift Algorithm/bulgi.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Algorithm
+//
+//  Created by Seungwon Choi on 2023/12/28.
+//
+
+import Foundation
+
+let bulgi = Int(readLine()!)!
+let seogi = bulgi - 543
+print(String(seogi))
