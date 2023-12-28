d235ac68a0f5bf3fd0819ed9496e203f7740a9a6
unuhqueen <csw98213@gmail.com>
2023-12-28 17:37:27 +0900

    10926번: ??!

---
+++ Swift Algorithm/Swift Algorithm/??!.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Swift Algorithm
+//
+//  Created by Seungwon Choi on 2023/12/20.
+//
+
+import Foundation
+
+let input = readLine()!
+print(input + "??!")
--- Swift Algorithm/Swift Algorithm/main.swift
+++ Swift Algorithm/Swift Algorithm/main.swift
@@ -2,7 +2,7 @@
 //  main.swift
 //  Swift Algorithm
 //
-//  Created by Seungwon Choi on 2023/12/20.
+//  Created by Seungwon Choi on 2023/12/28.
 //
 
 import Foundation
