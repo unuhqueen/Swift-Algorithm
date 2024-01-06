3926cf97b58eeabb68678fa6859a0ff5749b8688
unuhqueen <csw98213@gmail.com>
2024-01-06 17:10:24 +0900

    10171번: 고양이

---
+++ Swift Algorithm/Swift Algorithm/cat.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Swift Algorithm
+//
+//  Created by Seungwon Choi on 2024/01/06.
+//
+
+import Foundation
+
+print(#"\    /\"#)
+print(" )  ( ')")
+print("(  /  )")
+print(#" \(__)|"#)
