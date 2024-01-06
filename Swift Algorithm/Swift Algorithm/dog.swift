b1eed935c5c3d0e7a7f4d639ba64a3fecf5fef3e
unuhqueen <csw98213@gmail.com>
2024-01-06 17:12:43 +0900

    10172번: 개

---
+++ Swift Algorithm/Swift Algorithm/dog.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Swift Algorithm
+//
+//  Created by Seungwon Choi on 2024/01/06.
+//
+
+import Foundation
+
+let dog = #"""
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+"""#
+
+print(dog)
