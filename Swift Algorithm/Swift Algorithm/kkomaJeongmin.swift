7f221ff60cffe2dbe2cf9e51d00be29026b6a5ba
unuhqueen <csw98213@gmail.com>
2024-01-06 17:00:55 +0900

    11382번: 꼬마 정민

---
+++ Swift Algorithm/Swift Algorithm/kkomaJeongmin.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Swift Algorithm
+//
+//  Created by Seungwon Choi on 2024/01/06.
+//
+
+import Foundation
+
+let inputs = readLine()!.components(separatedBy: " ")
+
+let input1 = Int(inputs[0])!
+let input2 = Int(inputs[1])!
+let input3 = Int(inputs[2])!
+
+print(input1 + input2 + input3)
