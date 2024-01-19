8470618158c733ad45d235b6d7b2075e4bb28d20
unuhqueen <csw98213@gmail.com>
2024-01-19 19:01:08 +0900

    8393번: 합

---
+++ Swift Algorithm/Swift Algorithm/sum.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Swift Algorithm
+//
+//  Created by Seungwon Choi on 2024/01/19.
+//
+
+import Foundation
+
+let input = Int(readLine()!)!
+var sum = 0
+
+for i in 1...input {
+    sum += i
+}
+
+print(sum)
