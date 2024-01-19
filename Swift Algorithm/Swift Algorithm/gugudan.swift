3b4c645736f1fa73a1423d4ea9d98a30daa09f3a
unuhqueen <csw98213@gmail.com>
2024-01-19 18:46:16 +0900

    2739번: 구구단

---
+++ Swift Algorithm/Swift Algorithm/gugudan.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Swift Algorithm
+//
+//  Created by Seungwon Choi on 2024/01/19.
+//
+
+import Foundation
+
+let dan = Int(readLine()!)!
+
+for i in 1...9 {
+    print("\(dan) * \(i) = \(dan * i)")
+}
