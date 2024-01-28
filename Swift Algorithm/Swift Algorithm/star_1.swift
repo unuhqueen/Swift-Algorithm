33eafdc34993177aaa56fdea41d437a8e29c0712
unuhqueen <csw98213@gmail.com>
2024-01-28 20:53:57 +0900

    2438번: 별 찍기 - 1

---
+++ Swift Algorithm/Swift Algorithm/star_1.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Swift Algorithm
+//
+//  Created by Seungwon Choi on 2024/01/28.
+//
+
+import Foundation
+
+let input = Int(readLine()!)!
+for i in 1...input {
+    print(String(repeating: "*", count: i))
+}
