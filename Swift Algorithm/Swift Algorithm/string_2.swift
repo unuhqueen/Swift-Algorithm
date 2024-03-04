b540219c3b05e121eb13563b24d81b72b1a4c8d0
unuhqueen <csw98213@gmail.com>
2024-03-04 23:43:50 +0900

    9086번: 문자열

---
+++ Swift Algorithm/Swift Algorithm/string_2.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Swift Algorithm
+//
+//  Created by Seungwon Choi on 2/26/24.
+//
+
+import Foundation
+
+let numberOfInput = Int(readLine()!)!
+
+for _ in 0..<numberOfInput {
+    let input = readLine()!
+    print("\(input.first!)\(input.last!)")
+}
