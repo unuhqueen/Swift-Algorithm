f20e33542a7e875bffe0c4c21be42d2b75d641df
unuhqueen <csw98213@gmail.com>
2024-04-30 14:53:08 +0900

    Linked List Question #1

---
+++ Swift Algorithm/Swift Algorithm/linkedList#1.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+/*
+ Write a function that determines the length of any linked list.
+ */
+
+class Node {
+    var data: Int
+    var next: Node?
+    
+    init(_ data: Int, _ next: Node? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
+
+/// Time: O(n)
+func length(_ head: Node?) -> Int {
+    // Do your work here...
+    var currentNode = head
+    var count = 0
+    
+    if head == nil {
+        return 0
+    }
+    
+    while currentNode != nil {
+        currentNode = currentNode?.next
+        count += 1
+    }
+    return count
+}
+
+// 1 2 3 4 5 6
+let node6 = Node(6)
+let node5 = Node(5, node6)
+let node4 = Node(4, node5)
+let node3 = Node(3, node4)
+let node2 = Node(2, node3)
+let node1 = Node(1, node2)
+
+print(length(nil)) // 0
+print(length(node1)) // 6
